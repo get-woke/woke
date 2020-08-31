8b58ef45f7f438adcb72093a4f94f402ba057ad2
Caitlin Elfring <celfring@gmail.com>
2020-08-31 18:34:20 -0400

    Add result package tests

---
+++ pkg/result/fileresult_test.go
@@ -0,0 +1,19 @@
+package result
+
+import (
+	"testing"
+
+	"github.com/get-woke/woke/pkg/rule"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileResult_String(t *testing.T) {
+	rs := FindResults(&rule.WhitelistRule, "my/file", "this has the term whitelist", 1)
+	fr := FileResults{Filename: "my/file", Results: rs}
+	assert.Equal(t, "my/file\n    my/file:1:18-my/file:1:27 warn       `whitelist` maybe be insensitive, use `allowlist` instead", fr.String())
+
+	rs = FindResults(&rule.WhitelistRule, "my/file", "this has no rule violations", 1)
+	fr = FileResults{Filename: "my/file", Results: rs}
+	assert.Equal(t, "my/file", fr.String())
+
+}
