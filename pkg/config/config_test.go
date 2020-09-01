e984f4daf55a504c100e0853ac0f15ed44543fb0
Caitlin Elfring <celfring@gmail.com>
2020-09-01 10:22:37 -0400

    test: Add config package tests

---
+++ pkg/config/config_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/get-woke/woke/pkg/rule"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConfig(t *testing.T) {
+	c, err := NewConfig("testdata/good.yaml")
+	assert.NoError(t, err)
+
+	expectedRules := []*rule.Rule{}
+	expectedRules = append(expectedRules, &rule.Rule{
+		Name:         "rule1",
+		Terms:        []string{"rule1"},
+		Alternatives: []string{"alt-rule1"},
+		Severity:     rule.SevWarn,
+	})
+	expectedRules = append(expectedRules, &rule.Rule{
+		Name:         "rule2",
+		Terms:        []string{"rule2", "rule-2"},
+		Alternatives: []string{"alt-rule2", "alt-rule-2"},
+		Severity:     rule.SevError,
+	})
+
+	expected := &Config{
+		Rules:       expectedRules,
+		IgnoreFiles: []string{"README.md", "pkg/rule/default.go", "testdata/good.yaml"},
+	}
+
+	assert.EqualValues(t, expected, c)
+
+	// Test when no config file is provided
+	c, err = NewConfig("")
+	assert.NoError(t, err)
+
+	expected = &Config{
+		Rules:       rule.DefaultRules,
+		IgnoreFiles: []string{},
+	}
+	assert.Equal(t, expected, c)
+}
