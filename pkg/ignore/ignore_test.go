75c2f0e1066f3ba36ddc7d582aefc55d844c2cc8
Caitlin Elfring <celfring@gmail.com>
2020-09-02 11:43:46 -0400

    test: fix ignore package tests

--- pkg/ignore/ignore_test.go
+++ pkg/ignore/ignore_test.go
@@ -7,16 +7,17 @@ import (
 )
 
 func TestIgnoreMatch(t *testing.T) {
-	i, err := NewIgnore("my/fles/*")
+	i, err := NewIgnore("my/files/*")
 	assert.NoError(t, err)
 	assert.NotNil(t, i)
 
 	assert.False(t, i.Match("not/foo"))
 	assert.True(t, i.Match("my/files/file1"))
+	assert.False(t, i.Match("my/files"))
 }
 
 func TestReadIgnoreFIle(t *testing.T) {
 	lines := readIgnoreFile("testdata/.gitignore")
 
-	assert.Equal(t, "*.DS_Store", lines)
+	assert.Equal(t, []string{"*.DS_Store"}, lines)
 }
