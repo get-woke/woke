c04f831d57247a413b28881cfda70816f67615f3
Caitlin Elfring <celfring@gmail.com>
2020-09-02 21:29:29 -0400

    test: config package

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,48 +1,72 @@
 package config
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/get-woke/woke/pkg/rule"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewConfig(t *testing.T) {
-	c, err := NewConfig("testdata/good.yaml")
-	assert.NoError(t, err)
+	t.Run("check-logger", func(t *testing.T) {
+		out := &bytes.Buffer{}
+		log.Logger = zerolog.New(out)
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 
-	expectedRules := []*rule.Rule{}
-	expectedRules = append(expectedRules, &rule.Rule{
-		Name:         "rule1",
-		Terms:        []string{"rule1"},
-		Alternatives: []string{"alt-rule1"},
-		Severity:     rule.SevWarn,
-	})
-	expectedRules = append(expectedRules, &rule.Rule{
-		Name:         "rule2",
-		Terms:        []string{"rule2", "rule-2"},
-		Alternatives: []string{"alt-rule2", "alt-rule-2"},
-		Severity:     rule.SevError,
+		_, err := NewConfig("testdata/good.yaml")
+		assert.NoError(t, err)
+
+		assert.Equal(t, `{"level":"debug","rules":["rule1","rule2","whitelist","blacklist","master-slave","slave","grandfathered","man hours","sanity check","dummy","he","guys"],"message":"rules enabled"}`+"\n", out.String())
 	})
 
-	expected := &Config{
-		Rules:       expectedRules,
-		IgnoreFiles: []string{"README.md", "pkg/rule/default.go", "testdata/good.yaml"},
-	}
+	t.Run("config-good", func(t *testing.T) {
+		c, err := NewConfig("testdata/good.yaml")
+		assert.NoError(t, err)
 
-	assert.EqualValues(t, expected, c)
+		expectedRules := []*rule.Rule{}
+		expectedRules = append(expectedRules, &rule.Rule{
+			Name:         "rule1",
+			Terms:        []string{"rule1"},
+			Alternatives: []string{"alt-rule1"},
+			Severity:     rule.SevWarn,
+		})
+		expectedRules = append(expectedRules, &rule.Rule{
+			Name:         "rule2",
+			Terms:        []string{"rule2", "rule-2"},
+			Alternatives: []string{"alt-rule2", "alt-rule-2"},
+			Severity:     rule.SevError,
+		})
 
-	// Test when no config file is provided
-	c, err = NewConfig("")
-	assert.NoError(t, err)
+		expectedRules = append(expectedRules, rule.DefaultRules...)
 
-	expectedEmpty := &Config{
-		Rules:       rule.DefaultRules,
-		IgnoreFiles: []string(nil),
-	}
-	assert.Equal(t, expectedEmpty, c)
+		expected := &Config{
+			Rules:       expectedRules,
+			IgnoreFiles: []string{"README.md", "pkg/rule/default.go", "testdata/good.yaml"},
+		}
 
-	missing, err := NewConfig("testdata/missing.yaml")
-	assert.Error(t, err)
-	assert.Equal(t, expectedEmpty, missing)
+		assert.EqualValues(t, expected.Rules, c.Rules)
+	})
+
+	t.Run("config-empty-missing", func(t *testing.T) {
+		// Test when no config file is provided
+		c, err := NewConfig("")
+		assert.NoError(t, err)
+
+		expectedEmpty := &Config{
+			Rules:       rule.DefaultRules,
+			IgnoreFiles: []string(nil),
+		}
+		assert.Equal(t, expectedEmpty, c)
+	})
+
+	t.Run("config-missing", func(t *testing.T) {
+		// Test when no config file is provided
+		c, err := NewConfig("testdata/missing.yaml")
+		assert.Error(t, err)
+		assert.Nil(t, c)
+	})
+
 }
