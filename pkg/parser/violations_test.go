9481ea22c9759c196ff8e6f2c51864f83e80ac05
Caitlin Elfring <celfring@gmail.com>
2020-10-27 11:45:49 -0400

    Bug fix: last line of a file that does not end in a new line is no longer skipped (#33)

--- pkg/parser/violations_test.go
+++ pkg/parser/violations_test.go
@@ -14,44 +14,77 @@ import (
 )
 
 func TestGenerateFileViolations(t *testing.T) {
-	f, err := newFile(t, " this has whitelist\n")
-	assert.NoError(t, err)
-
-	res, err := generateFileViolationsFromFilename(f.Name(), rule.DefaultRules)
-	assert.NoError(t, err)
-
-	filename := filepath.ToSlash(f.Name())
-	expected := &result.FileResults{
-		Filename: filename,
-		Results:  make([]result.Result, 1),
+	tests := []struct {
+		desc    string
+		content string
+		line    string
+		start   int
+		end     int
+	}{
+		{"leading whitespace", " this has whitelist\n", " this has whitelist", 10, 19},
+		{"no leading whitespace", "this has whitelist\n", "this has whitelist", 9, 18},
+		{"leading whitespace, no new line", " this has whitelist", " this has whitelist", 10, 19},
+		{"no leading whitespace, no new line", "this has whitelist", "this has whitelist", 9, 18},
 	}
-	expected.Results[0] = result.LineResult{
-		Rule:      &rule.WhitelistRule,
-		Violation: "whitelist",
-		Line:      " this has whitelist",
-		StartPosition: &token.Position{
-			Filename: filename,
-			Offset:   0,
-			Line:     1,
-			Column:   10,
-		},
-		EndPosition: &token.Position{
-			Filename: filename,
-			Offset:   0,
-			Line:     1,
-			Column:   19,
-		},
-	}
-	assert.EqualValues(t, expected, res)
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			f, err := newFile(t, tc.content)
+			assert.NoError(t, err)
 
-	_, err = generateFileViolationsFromFilename("missing.file", rule.DefaultRules)
-	assert.Error(t, err)
+			res, err := generateFileViolationsFromFilename(f.Name(), rule.DefaultRules)
+			assert.NoError(t, err)
+
+			filename := filepath.ToSlash(f.Name())
+			expected := &result.FileResults{
+				Filename: filename,
+				Results:  make([]result.Result, 1),
+			}
+			expected.Results[0] = result.LineResult{
+				Rule:      &rule.WhitelistRule,
+				Violation: "whitelist",
+				Line:      tc.line,
+				StartPosition: &token.Position{
+					Filename: filename,
+					Offset:   0,
+					Line:     1,
+					Column:   tc.start,
+				},
+				EndPosition: &token.Position{
+					Filename: filename,
+					Offset:   0,
+					Line:     1,
+					Column:   tc.end,
+				},
+			}
+			assert.EqualValues(t, expected, res)
+		})
+	}
+	t.Run("missing file", func(t *testing.T) {
+		_, err := generateFileViolationsFromFilename("missing.file", rule.DefaultRules)
+		assert.Error(t, err)
+	})
+
+	t.Run("filename violation", func(t *testing.T) {
+		f, err := newFileWithPrefix(t, "whitelist-", "content")
+		assert.NoError(t, err)
+
+		res, err := generateFileViolationsFromFilename(f.Name(), rule.DefaultRules)
+		assert.NoError(t, err)
+		assert.Len(t, res.Results, 1)
+		assert.Regexp(t, "^Filename violation: ", res.Results[0].Reason())
+	})
 }
 
 // newFile creates a new file for testing. The file, and the directory that the file
 // was created in will be removed at the completion of the test
 func newFile(t *testing.T, text string) (*os.File, error) {
-	tmpFile, err := ioutil.TempFile(os.TempDir(), "woke-")
+	return newFileWithPrefix(t, "woke-", text)
+}
+
+// newFile creates a new file with the prefix defined for testing.
+// The file, and the directory that the file was created in will be removed at the completion of the test
+func newFileWithPrefix(t *testing.T, prefix, text string) (*os.File, error) {
+	tmpFile, err := ioutil.TempFile(os.TempDir(), prefix)
 	if err != nil {
 		return nil, err
 	}
