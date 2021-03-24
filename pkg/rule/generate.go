1559f77241b5f32ee84a8dcb5158633fa3ec2d71
Caitlin Elfring <celfring@gmail.com>
2021-03-24 08:56:55 -0400

    Use go:embed package for loading default rules from yaml

--- pkg/rule/generate.go
+++
@@ -1,4 +0,0 @@
-package rule
-
-//go:generate go run -tags codegen ../../internal/rule/gen.go -config ../../internal/rule/default.yaml -outfile ../rule/default.go
-//go:generate gofmt -s -w ../rule/default.go
