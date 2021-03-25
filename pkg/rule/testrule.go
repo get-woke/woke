32c93e111334e63e04468466d966428aaf1d05df
Caitlin Elfring <celfring@gmail.com>
2021-03-24 22:12:06 -0400

    wokeignore

--- pkg/rule/testrule.go
+++ pkg/rule/testrule.go
@@ -3,8 +3,8 @@ package rule
 // TestRule is only meant to be used in tests.
 // TODO: Use test terms
 var TestRule = Rule{
-	Name:         "whitelist",
-	Terms:        []string{"whitelist", "white-list", "whitelisted", "white-listed"},
+	Name:         "whitelist",                                                        // wokeignore:rule=whitelist
+	Terms:        []string{"whitelist", "white-list", "whitelisted", "white-listed"}, // wokeignore:rule=whitelist
 	Alternatives: []string{"allowlist"},
 	Severity:     1,
 	Options: Options{
