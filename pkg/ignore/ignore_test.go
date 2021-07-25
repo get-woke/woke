8e10219d47c75f53c3055a87b695174bb8d456e0
Nathan Byrd <techy1@gmail.com>
2021-07-25 16:44:38 -0500

    Added .ignore and tests for default ignore files

--- pkg/ignore/ignore_test.go
+++ pkg/ignore/ignore_test.go
@@ -1,6 +1,7 @@
 package ignore
 
 import (
+	"os"
 	"runtime"
 	"testing"
 
@@ -24,6 +25,34 @@ func TestIgnore_Match(t *testing.T) {
 		assert.False(t, i.Match(`not\foo`))
 		assert.True(t, i.Match(`my\files\file1`))
 		assert.False(t, i.Match(`my\files`))
+	}
+}
+
+// Test all default ignore files, except for .git/info/exclude, since
+// that uses a .git directory that we cannot check in.
+func TestIgnoreDefaultIgoreFiles_Match(t *testing.T) {
+
+	// Change into testdata directory for this test
+	// since paths are relative
+	err := os.Chdir("testdata")
+	if err != nil {
+		panic(err)
+	}
+
+	i := NewIgnore([]string{"*.FROMARGUMENT"})
+	assert.NotNil(t, i)
+
+	assert.False(t, i.Match("notfoo"))
+	assert.True(t, i.Match("test.FROMARGUMENT")) // From .gitignore
+	assert.True(t, i.Match("test.DS_Store"))     // From .gitignore
+	assert.True(t, i.Match("test.IGNORE"))       // From .ignore
+	assert.True(t, i.Match("test.WOKEIGNORE"))   // From .wokeignore
+	assert.False(t, i.Match("test.NOTIGNORED"))  // From .notincluded - making sure only default are included
+
+	// Change directory back to original directory
+	err = os.Chdir("..")
+	if err != nil {
+		panic(err)
 	}
 }
 
