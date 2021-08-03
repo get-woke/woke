26f302b83d18bcdb7ecf6c79265a49bb2d48b2e3
Mary Kate Comer <mkcomer@RYPETERS-CON.redmond.corp.microsoft.com>
2021-08-03 09:53:45 -0400

    Added remote ruleset logic

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 
@@ -81,12 +84,68 @@ func (c *Config) ConfigureRules() {
 }
 
 func loadConfig(filename string) (c Config, err error) {
+	if isValidUrl(filename) {
+		// if fileName is a valid URL, we will download and set it to the config
+		log.Debug().Str("url", filename).Msg("Downloading file from")
+		//hardcoding this file and saving to root directory
+		downloadedFile := "downloadedRules.yaml"
+		err := DownloadFile(downloadedFile, filename)
+		if err != nil {
+			return c, err
+		}
+		filename = downloadedFile
+		log.Debug().Str("filename", filename).Msg("Saved remote config to local file.")
+	}
+
+	// TO DO = Need to add more error handling on reading/validating yamlFile file - issue # 16
 	yamlFile, err := ioutil.ReadFile(filename)
+	log.Debug().Str("filename", filename).Msg("Adding custom ruleset from")
 	if err != nil {
 		return c, err
 	}
-
 	return c, yaml.Unmarshal(yamlFile, &c)
+}
+
+// isValidUrl tests a string to determine if it is a valid URL or not
+func isValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(toTest)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		return false
+	}
+	log.Debug().Str("remoteConfig", toTest).Msg("Valid URL for remote config.")
+	return true
+}
+
+//downloads file from url to set filepath
+func DownloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	// only parse response body if it is in the response is in the 2xx range
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		log.Debug().Int("HTTP Response Status:", resp.StatusCode).Msg("Valid URL Response")
+		defer resp.Body.Close()
+
+		// Create the file
+		out, err := os.Create(filepath)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		// Write the body to file
+		_, err = io.Copy(out, resp.Body)
+		return err
+	} else {
+		return fmt.Errorf("unable to download remote config from url - http response code: %v", resp.StatusCode)
+	}
 }
 
 func relative(filename string) string {
